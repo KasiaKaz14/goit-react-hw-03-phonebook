f010525c69b0ffaa2f41388e4cacc81418a4f855
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-09-20 21:46:51 +0200

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,6 +21,19 @@ export class App extends Component {
 
   nanoid = nanoid();
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSubmit = evt => {
     evt.preventDefault();
     const form = evt.target;
